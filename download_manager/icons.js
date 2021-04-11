b5b9291d9a39da3ce46f2b9422a6ee0a9ee4d4a3
Rahul H Gupta <67586773+rahulhgdev@users.noreply.github.com>
2021-04-11 19:53:03 +0530

    added all files related to Download Manager

---
+++ download_manager/icons.js
@@ -0,0 +1,10 @@
+
+
+window.onload = function() {
+  var download = document.getElementById('download');
+  download.onclick = function() {
+    chrome.runtime.sendMessage('icons');
+    download.disabled = true;
+    return false;
+  };
+};
